6bea9e9210fa6315c6626e6b279127e9eaf54f87
Alex Carrillo <carrilloa01@gmail.com>
2019-02-08 18:33:00 -0800

    added the students and clear buttons

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,23 @@
 import 'materialize-css/dist/css/materialize.min.css';
+import'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
 import React from 'react';
+import Addstudent from './add_students';
 import Table from './table';
-
-
 
 const App = () => (
     <div>
         <h1 className="center">SGT</h1>
 
-        <Table/>
+        <div className="row">
+            <div className='row'>
+                <Table/>
+            </div>
+            <div className="col s12 m4">
+                <Addstudent/>
+            </div>
+        </div>
+
     </div>
 );
 
