7adfba868afa391c301b56c32ee3c05d64f0a3b5
Alex Carrillo <carrilloa01@gmail.com>
2019-02-13 18:11:40 -0800

    set student page and routing for individual student

--- src/components/add_students.js
+++ src/components/add_students.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {formatPostData} from "../helpers";
+import { Link} from "react-router-dom";
+import axios from "axios";
 
 class AddStudent extends Component {
     state = {
@@ -9,12 +12,14 @@ class AddStudent extends Component {
         notes: ''
     }
 
-    handleSubmit = ( event ) => {
+    handleSubmit = async ( event ) => {
         event.preventDefault();
 
-        this.props.add(this.state);
+        const formattedStudent = formatPostData(this.state);
 
-        this.resetForm();
+        await axios.post('/server/createstudent.php', formattedStudent);
+
+        this.props.history.push('/');
     }
 
     handleKeyPress = (event) => {
@@ -58,48 +63,60 @@ class AddStudent extends Component {
     render(){
         const {name, course, grade, instructor, notes} = this.state;
 
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <div className="row">
-                    <div className='col input-field s10 offset-s1'>
-                        <input onChange={this.handleKeyPress} name="name" type="text" value={name} autoComplete="off"/>
-                        <label htmlFor="name">Name</label>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className='col input-field s10 offset-s1'>
-                        <input onChange={this.handleKeyPress} name="course" type="text" value={course}/>
-                        <label htmlFor="course">Course</label>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className='col input-field s10 offset-s1'>
-                        <input onChange={this.handleKeyPress} name="grade" type="text" value={grade}/>
-                        <label htmlFor="grade">Grade</label>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className='col input-field s10 offset-s1'>
-                        <input onChange={this.handleKeyPress} name="instructor" type="number" type="text" value={instructor}/>
-                        <label htmlFor="instructor">Instructor</label>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className='col input-field s10 offset-s1'>
-                        <input onChange={this.handleKeyPress} name="notes" type="number" type="text" value={notes}/>
-                        <label htmlFor="notes">Notes</label>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col s6 center">
-                        <button onClick={this.resetForm} type="button" className="btn red darken-2 waves-effect waves-light">Clear</button>
-                    </div>
-                    <div className="col s6 center">
-                        <button className='btn green darken-2'>Add</button>
-                    </div>
 
+        return (
+            <div>
+                <h1 className="center">Add student</h1>
+
+                <div className="row">
+                    <div className="col s12 right-align">
+                        <Link className='btn blue' to="/">Home</Link>
+                        </div>
                 </div>
-            </form>
+
+                <form onSubmit={this.handleSubmit}>
+                    <div className="row">
+                        <div className='col input-field s10 offset-s1'>
+                            <input onChange={this.handleKeyPress} name="name" type="text" value={name} autoComplete="off"/>
+                            <label htmlFor="name">Name</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className='col input-field s10 offset-s1'>
+                            <input onChange={this.handleKeyPress} name="course" type="text" value={course}/>
+                            <label htmlFor="course">Course</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className='col input-field s10 offset-s1'>
+                            <input onChange={this.handleKeyPress} name="grade" type="text" value={grade}/>
+                            <label htmlFor="grade">Grade</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className='col input-field s10 offset-s1'>
+                            <input onChange={this.handleKeyPress} name="instructor" type="number" type="text" value={instructor}/>
+                            <label htmlFor="instructor">Instructor</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className='col input-field s10 offset-s1'>
+                            <input onChange={this.handleKeyPress} name="notes" type="number" type="text" value={notes}/>
+                            <label htmlFor="notes">Notes</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col s6 center">
+                            <button onClick={this.resetForm} type="button" className="btn red darken-2 waves-effect waves-light">Clear</button>
+                        </div>
+                        <div className="col s6 center">
+                            <button className='btn green darken-2'>Add</button>
+                        </div>
+
+                    </div>
+                </form>
+
+            </div>
         );
     }
 
--- src/components/app.js
+++ src/components/app.js
@@ -2,90 +2,26 @@ import 'materialize-css/dist/css/materialize.min.css';
 import'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Route } from 'react-router-dom';
 import Addstudent from './add_students';
 import Table from './table';
-import { formatPostData } from '../helpers';
+import ViewStudent from './view_student';
+
 
 
 class App extends Component {
-    state = {
-        students: []
-    }
 
-    componentDidMount() {
-        this.getStudentData();
-
-    }
-
-    deleteStudent = async (id) => {
-
-        const formattedId = formatPostData({ id });
-
-        await axios.post('/server/deletestudent.php', formattedId);
-
-        this.getStudentData();
-
-    }
-
-
-    addStudent = async (student) =>{
-
-        const formattedStudent = formatPostData(student);
-
-        await axios.post('/server/createstudent.php', formattedStudent);
-
-        this.getStudentData();
-
-    }
-
-    async getStudentData(){
-
-        const resp = await axios.get('/server/getstudentlist.php');
-
-        this.setState({
-            students: resp.data.data || []
-        });
-
-        // if (resp.data.success){
-        //     this.setState({
-        //         students: resp.data.data
-        //     });
-        // } else {
-        //     this.setState( {
-        //         students: []
-        //     })
-        // }
-
-        // call server to get student data
-        // axios.get('http://localhost/server/getstudentlist.php').then((response) => {
-        //     console.log("Server Response:", response.data.data);
-        //
-        //     this.setState({
-        //         students: response.data.data
-        //     });
-        // });
-        //
-
-    }
 
     render() {
         return (
-            <div>
-                <h1 className="center">SGT</h1>
+            <div className='container'>
 
-                <div className="row">
-                    <div className='col s12 m8'>
-                        <Table deleteStudent={this.deleteStudent} studentList={this.state.students}/>
-                    </div>
-                    <div className="col s12 m4">
-                        <Addstudent add={this.addStudent}/>
-                    </div>
-                </div>
-
+                <Route exact path="/" component={Table}/>
+                <Route path="/add-student" component={Addstudent}/>
+                <Route path="/student/:id" component={ViewStudent}/>
             </div>
-        );
+          );
+        }
     }
-}
 
 export default App;
